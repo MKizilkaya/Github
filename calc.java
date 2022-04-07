9256199820656cf04352aa7751088c68c42f69d0
Mehmet Kizilkaya <Mehmet Kizilkaya>
2022-04-07 22:23:21 +0300

    version1

---
+++ calc.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class calc{
+    public static void main(String[] args) {
+        double sayi1,sayi2;
+        int secim;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Birinci sayiyi giriniz:");
+        sayi1=scan.nextDouble();
+        System.out.println("Ikinci sayiyi giriniz:");
+        sayi2=scan.nextDouble();
+        System.out.println("Islem seciniz:\n1-Toplama\n2-Cikarma");
+        secim=scan.nextInt();
+        if(secim==1)
+        System.out.println("Sonuc:"+(sayi1+sayi2));
+        else if(secim==2)
+        System.out.println("Sonuc:"+(sayi1-sayi2));
+        else
+        System.out.println("HATA");
+
+    }
+}
