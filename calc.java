28166814d2d013b57fa0ec5e143622c63ee436c5
Mehmet Kizilkaya <Mehmet Kizilkaya>
2022-04-07 22:32:35 +0300

    carpma bolme eklendi

--- calc.java
+++ calc.java
@@ -8,12 +8,16 @@ public class calc{
         sayi1=scan.nextDouble();
         System.out.println("Ikinci sayiyi giriniz:");
         sayi2=scan.nextDouble();
-        System.out.println("Islem seciniz:\n1-Toplama\n2-Cikarma");
+        System.out.println("Islem seciniz:\n1-Toplama\n2-Cikarma\n3-Carpma\n4-Bolme");
         secim=scan.nextInt();
         if(secim==1)
         System.out.println("Sonuc:"+(sayi1+sayi2));
         else if(secim==2)
         System.out.println("Sonuc:"+(sayi1-sayi2));
+        else if(secim==3)
+        System.out.println("Sonuc:"+(sayi1*sayi2));
+        else if(secim==4)
+        System.out.println("Sonuc:"+(sayi1/sayi2));
         else
         System.out.println("HATA");
 
